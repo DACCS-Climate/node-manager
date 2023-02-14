109e762ef1ac781c7937224b2a90ae1ee504bb5c
alexandercyu <alexanderc.yu@utoronto.ca>
2023-02-14 14:58:47 -0500

    Add main.py
    - defines HTTP methods for web interface
    
    - GET methods added

---
+++ main.py
@@ -0,0 +1,41 @@
+from fastapi import FastAPI
+
+# Creat an instance of the class FastAPI
+# This will be the same instance referenced by uvicorn in the command "uvicorn main:app --reload"
+app=FastAPI()
+
+# GET 
+@app.get("/")
+async def root():
+    return {"message":"Main Page"}
+
+
+app.get("/docs")
+async def docs():
+    return {"message":"Docs Page"}
+
+app.get("/docs/jupyter")
+async def jupyterdocs():
+    return {"message":"Jupyter Page"}
+
+
+app.get("/login")
+async def login():
+    return {"message":"Login Page"}
+
+app.get("/admin")
+async def admin():
+    return {"message":"Admin Page"}
+
+
+app.get("/catalog")
+async def catalog():
+    return {"message":"Catalog Page"}
+
+app.get("/weaver")
+async def weaver():
+    return {"message":"Weaver Page"}
+
+
+
+    
