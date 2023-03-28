5876106d96a8f00f45e38db7facbf7d72e7ebd66
alexandercyu <alexanderc.yu@utoronto.ca>
2023-03-28 11:48:32 -0400

    Move __init__.py
    Move home/__init__.py to go under database folder

---
+++ database/home/__init__.py
@@ -0,0 +1,33 @@
+from pyramid.config import Configurator
+
+
+
+
+def main(global_config, **settings):
+
+    # Create a 'settings file' called 'home'
+    # You can add settings to this through the Configurator functions
+    config = Configurator(settings=settings)
+
+    # Include Chameleon as a template renderer
+    config.include('pyramid_chameleon')
+
+    # Add a route / path called home at the root
+    # Full route / path should be /home
+    config.add_route('node_home', '/node')
+    #homeURL = request.route_url('homeURL')
+
+
+
+
+    # Add a route / path called node_docs at the /node/docs
+    config.add_route("node_docs", "/node/docs")
+
+
+
+
+
+
+
+
+    return config.make_wsgi_app()
