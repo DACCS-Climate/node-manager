100d6383ded7d604ff06faaa193d03effa564ac9
alexandercyu <alexanderc.yu@utoronto.ca>
2023-03-31 16:43:36 -0400

    Update __init__.py
    Commented out routes for 'node_info' and 'node_register' because those will be accessible through api

--- network_manager/__init__.py
+++ network_manager/__init__.py
@@ -10,20 +10,21 @@ def main(global_config, **settings):
     Base.metadata.bind = engine
 
     config = Configurator(settings=settings, root_factory=".models.Root")
+    config.include("cornice")
     config.include("pyramid_chameleon")
 
     # Add a route / path called node_home at /node for displaying all entries in the database
     config.add_route("node_home", "/node")
 
-    # Add a route / path called node_register at /node/register.  This is the 'admin' page where an entry can be added
-    # to the database
-    config.add_route("node_register", "/node/register", request_method="GET")
+    # Add a route / path called node_register at /node/register.
+    # This is the 'admin' page where an entry can be added to the database
+    # config.add_route("node_register", "/node/register", request_method="GET")
 
     # Add a route / path for the particular node {node_id} at /node/info/{node_id}
-    # Returns details about a particular node in json
-    config.add_route("node_info", "/node/info/{node_id}", request_method="GET")
+    # Returns details about a particular node in json format
+    # config.add_route("node_info", "/node/info/{node_id}", request_method="GET")
 
-    config.add_static_view("deform_static", "deform:static/")
+    # config.add_static_view("deform_static", "deform:static/")
 
     config.scan(".views")
 
