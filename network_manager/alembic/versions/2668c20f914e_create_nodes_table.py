cc7e5aa985229c138e7bcd25897d854eed840bf6
alexandercyu <alexanderc.yu@utoronto.ca>
2023-04-05 17:07:10 -0400

    alembic nodes table scripts
    Created upgrade and downgrade functions for 'nodes' table for alembic

---
+++ network_manager/alembic/versions/2668c20f914e_create_nodes_table.py
@@ -0,0 +1,24 @@
+"""create nodes table
+
+Revision ID: 2668c20f914e
+Revises:
+Create Date: 2023-04-04 15:48:46.205580
+
+"""
+# from alembic import op
+# import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = "2668c20f914e"
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
---
+++ network_manager/alembic/versions/621a290666b8_create_nodes_table.py
@@ -0,0 +1,35 @@
+"""create nodes table
+
+Revision ID: 621a290666b8
+Revises: 2668c20f914e
+Create Date: 2023-04-05 16:05:39.918614
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = "621a290666b8"
+down_revision = "2668c20f914e"
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    op.create_table(
+        "nodes",
+        sa.Column("node_id", sa.Integer, primary_key=True, autoincrement=True),
+        sa.Column("node_name", sa.String(50), nullable=False),
+        sa.Column("node_description", sa.String(200)),
+        sa.Column("location", sa.String(50)),
+        sa.Column("affiliation", sa.String(50)),
+        sa.Column("url", sa.String(50)),
+        sa.Column("capabilities", sa.String(200)),
+        sa.Column("user_email", sa.String(50)),
+        sa.Column("deploy_start_date", sa.Date),
+    )
+
+
+def downgrade() -> None:
+    op.drop_table("nodes")
