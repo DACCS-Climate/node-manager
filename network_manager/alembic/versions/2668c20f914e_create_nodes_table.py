dd0a980c9d6da3f859c71ff8152de0ae8392e1ff
alexandercyu <alexanderc.yu@utoronto.ca>
2023-04-12 16:19:42 -0400

    Deleted unneeded file
    File not used anymore

--- network_manager/alembic/versions/2668c20f914e_create_nodes_table.py
+++ network_manager/alembic/versions/2668c20f914e_create_nodes_table.py
@@ -5,8 +5,8 @@ Revises:
 Create Date: 2023-04-04 15:48:46.205580
 
 """
-# from alembic import op
-# import sqlalchemy as sa
+from alembic import op
+import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
@@ -17,8 +17,21 @@ depends_on = None
 
 
 def upgrade() -> None:
-    pass
+    op.create_table(
+        "nodes",
+        sa.Column("node_id", sa.Integer, primary_key=True, autoincrement=True),
+        sa.Column("node_name", sa.String(50), nullable=False),
+        sa.Column("node_description", sa.String(200)),
+        sa.Column("location", sa.String(50)),
+        sa.Column("affiliation", sa.String(50)),
+        sa.Column("url", sa.String(50)),
+        sa.Column("support_contact_email", sa.String(50)),
+        sa.Column("deploy_start_date", sa.Date),
+        sa.Column("data", sa.Boolean(), nullable=True),
+        sa.Column("compute", sa.Boolean(), nullable=True),
+        sa.Column("administrator", sa.String(100), nullable=True),
+    )
 
 
 def downgrade() -> None:
-    pass
+    op.drop_table("nodes")
