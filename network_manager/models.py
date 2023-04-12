d0739103c672cf6c19eb36ee8ec95f43c74bed50
alexandercyu <alexanderc.yu@utoronto.ca>
2023-04-12 16:28:25 -0400

    Update models.py
     - Removed unneeded column definitions
     - Added other needed column definitions

--- network_manager/models.py
+++ network_manager/models.py
@@ -15,14 +15,13 @@ from sqlalchemy.orm import (
     sessionmaker,
 )
 
-
 DBSession = scoped_session(sessionmaker())
-
 register(DBSession)
 
 Base = declarative_base()
 
 
+# Define model for node table
 class Node(Base):
 
     __tablename__ = "nodes"
@@ -34,16 +33,17 @@ class Node(Base):
     location = Column(Text)
     affiliation = Column(Text)
     url = Column(Text, unique=True)
-    weaver = Column(Boolean, default=False)
-    jupyter = Column(Boolean, default=False)
-    catalog = Column(Boolean, default=False)
+    support_contact_email = Column(Text)
     deploy_start_date = Column(DateTime)
-    user_email = Column(Text)
+    data = Column(Boolean, default=False)
+    compute = Column(Boolean, default=False)
+    administrator = Column(Text)
 
 
+# Root Factory
+# Used for building access control list
 class Root:
-
     __acl__ = [(Allow, Everyone, "view"), (Allow, "group:editors", "edit")]
 
     def __init__(self, request):
-        pass
+        self.request = request
