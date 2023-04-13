797f71291c7aa9117fa716b152963f7d63a6b896
alexandercyu <alexanderc.yu@utoronto.ca>
2023-04-13 17:14:05 -0400

    Update constants.py
    Remove sqlite connection string

--- network_manager/constants.py
+++ network_manager/constants.py
@@ -1,6 +1,5 @@
 # Database connection strings
 NM_POSTGRES_DATABASE_URL = "postgresql+psycopg2://postgres:localpassword@localhost:5432/noderegistry"
-NM_SQLITE_DATABASE_URL = "sqlite:///%(here)s/sqltutorial.sqlite"
 
 # Github url variables
 NM_NODE_REGISTRY_URL = "https://raw.githubusercontent.com/DACCS-Climate/DACCS-node-registry/main/node_registry.json"
