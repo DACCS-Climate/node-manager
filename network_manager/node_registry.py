ee5584b2daaac8eb6c8b7e222674acc6eb70133c
alexandercyu <alexanderc.yu@utoronto.ca>
2023-04-13 15:50:46 -0400

    Update node_registry.py
    Add function to get node_info.json

--- network_manager/node_registry.py
+++ network_manager/node_registry.py
@@ -1,6 +1,6 @@
 import requests
 import json
-from .constants import NM_NODE_REGISTRY_URL
+from .constants import NM_NODE_REGISTRY_URL, NM_NODE_INFO_URL
 
 
 class NodeRegistry:
@@ -13,3 +13,11 @@ class NodeRegistry:
         node_registry_json = json.loads(node_registry_file.text)
 
         return node_registry_json
+
+    def get_node_info(self):
+
+        node_info_url = NM_NODE_INFO_URL
+        node_info_file = requests.get(node_info_url, allow_redirects=True)
+        node_info_json = json.loads(node_info_file.text)
+
+        return node_info_json
