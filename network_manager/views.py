28f6d509b586f24474f9bdbba175a4d556301890
alexandercyu <alexanderc.yu@utoronto.ca>
2023-04-14 12:42:13 -0400

    Update views.py
    - Added blank defaults for loading empty form fields
    - Changed node_home() to display_all_nodes() for displaying a blank form with list of all nodes for when user is visiting page for the first time

--- network_manager/views.py
+++ network_manager/views.py
@@ -18,18 +18,41 @@ class NodeViews:
     @property
     def node_form(self):
         # On page load get the passed node ID and get the node details to populate the form fields
-        requested_node_id = int(self.request.matchdict["node_id"])
-        requested_node = DBSession.query(Node).filter_by(node_id=requested_node_id).one()
+
+        # If no node_id is passed, form defaults are blank
+        node_name_default = ""
+        node_url_default = ""
+        node_location_default = ""
+        node_affiliation_default = ""
+        node_administrator_default = ""
+        node_deploy_date_default = ""
+        node_support_contact_default = ""
+        node_capability_data_default = ""
+        node_capability_compute_default = ""
+
+        # If node_id is passed get the node details to display in form
+        if len(self.request.matchdict) > 0:
+            requested_node_id = int(self.request.matchdict["node_id"])
+            requested_node = DBSession.query(Node).filter_by(node_id=requested_node_id).one()
+            node_name_default = requested_node.node_name
+            node_url_default = requested_node.url
+            node_location_default = requested_node.location
+            node_affiliation_default = requested_node.affiliation
+            node_administrator_default = requested_node.administrator
+            node_deploy_date_default = requested_node.deploy_start_date
+            node_support_contact_default = requested_node.support_contact_email
+            node_capability_data_default = requested_node.data
+            node_capability_compute_default = requested_node.compute
 
         # Used for Node Update form
         class NodeForm(colander.MappingSchema):
 
-            node_name = colander.SchemaNode(colander.String(), default=requested_node.node_name)
-            node_url = colander.SchemaNode(colander.String(), default=requested_node.url)
-            location = colander.SchemaNode(colander.String(), missing="", default=requested_node.location)
-            affiliation = colander.SchemaNode(colander.String(), missing="", default=requested_node.affiliation)
+            node_name = colander.SchemaNode(colander.String(), default=node_name_default)
+            node_url = colander.SchemaNode(colander.String(), default=node_url_default)
+            location = colander.SchemaNode(colander.String(), missing="", default=node_location_default)
+            affiliation = colander.SchemaNode(colander.String(), missing="", default=node_affiliation_default)
             administrator = colander.SchemaNode(
-                colander.String(), missing="", default=requested_node.administrator, title="Administrator"
+                colander.String(), missing="", default=node_administrator_default, title="Administrator"
             )
             deployed_since = colander.SchemaNode(
                 colander.Date(),
@@ -38,17 +61,17 @@ class NodeViews:
                     min=datetime.date(2010, 1, 1),
                 ),
                 missing="",
-                default=requested_node.deploy_start_date,
+                default=node_deploy_date_default,
                 title="Deployed Since",
             )
             support_contact_email = colander.SchemaNode(
-                colander.String(), missing="", default=requested_node.support_contact_email
+                colander.String(), missing="", default=node_support_contact_default
             )
             data = colander.SchemaNode(
                 colander.Boolean(),
                 widget=deform.widget.CheckboxWidget(),
                 missing="",
-                default=requested_node.data,
+                default=node_capability_data_default,
                 label="Data",
                 title="Capabilities",
             )
@@ -56,7 +79,7 @@ class NodeViews:
                 colander.Boolean(),
                 widget=deform.widget.CheckboxWidget(),
                 missing="",
-                default=requested_node.compute,
+                default=node_capability_compute_default,
                 label="Compute",
             )
 
@@ -69,20 +92,28 @@ class NodeViews:
     def reqts(self):
         return self.node_form.get_widget_resources()
 
-    @view_config(route_name="node_home", renderer="templates/node_home.pt")
-    def node_home(self):
+    @view_config(route_name="node_all", renderer="templates/node_update.pt")
+    def display_all_nodes(self):
+        form = self.node_form.render()
 
+        # Get information from github node registry and display list of all nodes
         db = DB()
         db.add_github_node_registry()
 
         db_contents = DBSession.query(Node).order_by(Node.node_id)
 
-        return dict(page_title="All Nodes", db_node=db_contents)
+        return dict(page_title="All Nodes", db_node=db_contents, form=form)
 
     @view_config(route_name="node_update", renderer="templates/node_update.pt")
     def node_update(self):
         title = "Node Update"
 
+        # Get information from github node registry and display list of all nodes
+        db = DB()
+        db.add_github_node_registry()
+        db_contents = DBSession.query(Node).order_by(Node.node_id)
+
+        # Get the specific node's details to display in form
         requested_node_id = int(self.request.matchdict["node_id"])
 
         form = self.node_form.render()
@@ -136,7 +167,7 @@ class NodeViews:
             url = self.request.route_url("node_update", node_id=new_node_id, page_title=title)
             return HTTPFound(url)
 
-        return dict(page_title=title, form=form)
+        return dict(page_title=title, db_node=db_contents, form=form)
 
     @view_config(route_name="node_info", renderer="json")
     def node_info_view(self):
